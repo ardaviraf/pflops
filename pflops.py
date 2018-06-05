6b5b4521616e95c48e49dac1b747f080bb1c7e4a
ardaviraf <ardaviraf@gmail.com>
2018-06-05 11:38:37 +0300

    Create pflops.py

---
+++ pflops.py
@@ -0,0 +1,16 @@
+float_increment = 1.03 # random
+start = 57.24 # random
+floating_point = start
+loop_range=10 ** 7
+
+for i in xrange(loop_range):
+    floating_point += float_increment
+    floating_point += float_increment
+    floating_point += float_increment
+    floating_point += float_increment
+    floating_point += float_increment
+    floating_point += float_increment
+    floating_point += float_increment
+    floating_point += float_increment
+    floating_point += float_increment
+    floating_point += float_increment
